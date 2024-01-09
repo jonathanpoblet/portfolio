170cb6289e42f99779cbc9014ebeecae11d80e0a
Jonathan Poblet <jonathanpoblet228@gmail.com>
2024-01-09 16:59:45 -0300

    Project Update

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -7,9 +7,9 @@ export default function Projects() {
             PROJECTS
         </h2>
         <div className='projects-container'>
-            <img onClick={() => window.location.href = 'https://jonathanpoblet.github.io/taraborelli-agro/'} className='projects-container-img' src='./assets/agro.png'  alt='project img' />
+            <img onClick={() => window.location.href = 'http://prueba.taraborelliagro.com/'} className='projects-container-img' src='./assets/agro.png'  alt='project img' />
             <img onClick={() => window.location.href = 'https://jonathanpoblet.github.io/jardineria/'} className='projects-container-img' src='./assets/botanica.png'  alt='project img2' />
-            <img onClick={() => window.location.href = 'https://jonathanpoblet.github.io/PasteleriaRiccione/'} className='projects-container-img' src='./assets/riccione.png'  alt='project img3' />
+            <img onClick={() => window.location.href = 'https://jonathanpoblet.github.io/forzagym/'} className='projects-container-img' src='./assets/forza.png'  alt='project img3' />
             <img onClick={() => window.location.href = 'https://jonathanpoblet.github.io/Taraborelli-Automobile/'} className='projects-container-img' src='./assets/automobile.png'  alt='project img4' />
         </div>
     </>
