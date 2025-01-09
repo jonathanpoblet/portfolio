0999b820346251a2dcdba00114a85f501ed1d59a
unknown <jonathanpoblet228@gmail.com>
2025-01-09 15:02:34 -0300

    Updated Portfolio Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,25 +3,25 @@ import './header.css';
 export default function Header() {
   return (
     <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">HOME</a>
-            </li>
-            
-            <li>
-              <a href="#resume">RESUME</a>
-            </li>
+      <nav>
+        <ul>
+          <li>
+            <a href='#home'>HOME</a>
+          </li>
 
-            <li>
-              <a href="#projects">PROJECTS</a>
-            </li>
+          <li>
+            <a href='#resume'>RESUME</a>
+          </li>
 
-            <li>
-              <a href="#contact">CONTACT</a>
-            </li>
-          </ul>
-        </nav>
+          <li>
+            <a href='#projects'>PROJECTS</a>
+          </li>
+
+          <li>
+            <a href='#contact'>CONTACT</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
--- src/components/Main/Contact/Contact.jsx
+++ src/components/Main/Contact/Contact.jsx
@@ -5,21 +5,30 @@ import './contact.css';
 export default function Contact() {
   return (
     <>
-        <h2 className='contact-title'>CONTACT</h2>
-        <div className='contact-container'>
-            <div className='contact-container-info'>
-                <BiPhone className='contact-container-info-icon'/>
-                <p className='contact-container-info-text'>+54 11 3056 5913</p>
-            </div>
-            <div className='contact-container-info'>
-                <SiGmail className='contact-container-info-icon'/>
-                <p className='contact-container-info-text'>jonathanpoblet228@gmail.com</p>
-            </div>
-            <div className='contact-container-info'>
-                <SiInstagram className='contact-container-info-icon'/>
-                <p className='contact-container-info-text'>@jonathan_poblet</p>
-            </div>
+      <h2 className='contact-title'>CONTACT</h2>
+      <div className='contact-container'>
+        <div
+          className='contact-container-info'
+          onClick={() => window.open('https://wa.me/541130565913', '_blank')}
+        >
+          <BiPhone className='contact-container-info-icon' />
+          <p className='contact-container-info-text'>+54 11 3056 5913</p>
         </div>
+        <div
+          className='contact-container-info'
+          onClick={() => window.open('mailto:contacto@virtualdesigns.tech')}
+        >
+          <SiGmail className='contact-container-info-icon' />
+          <p className='contact-container-info-text'>contacto@virtualdesigns.tech</p>
+        </div>
+        <div
+          className='contact-container-info'
+          onClick={() => window.open('https://www.instagram.com/jonathan_poblet/', '_blank')}
+        >
+          <SiInstagram className='contact-container-info-icon' />
+          <p className='contact-container-info-text'>@jonathan_poblet</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
